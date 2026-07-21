7d20cc6f8ff437b0b129610323cff39dfe8a72d7
TheBarli <barligsetiawan@gmail.com>
2026-07-21 20:58:50 +0700

    Dependency and Base - Adding all dependency (react, vite, breeze, laravel, sanctum) - creating base for developing

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react'; // <-- 1. Import plugin React
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'], // <-- 2. Ubah app.js jadi app.jsx
+            input: 'resources/js/app.jsx',
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
         }),
-        tailwindcss(),
-        react(), // <-- 3. Masukkan plugin React ke dalam array plugins
+        react(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
