65280a5826cffbd911f51f4aa150d16aab4f2c83
Daffa Ap <daffaap655@gmail.com>
2026-07-21 18:38:25 +0700

    Instalasi Sanctum, sama React

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react'; // <-- 1. Import plugin React
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'], // <-- 2. Ubah app.js jadi app.jsx
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
@@ -15,6 +16,7 @@ export default defineConfig({
             ],
         }),
         tailwindcss(),
+        react(), // <-- 3. Masukkan plugin React ke dalam array plugins
     ],
     server: {
         watch: {
